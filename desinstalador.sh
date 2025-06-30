20c6e4bb0db2b849d9b813320d03dbe8c7bd07d6
Soporte Telnet <soporte.ti@telnetsp.com>
2025-06-30 17:10:00 -0500

    feat: se actualiza desinstalador para agregar eliminar pods creados por usuarioo por jupyterhub

--- desinstalador.sh
+++ desinstalador.sh
@@ -15,22 +15,26 @@ helm uninstall postgresql-jupyterhub || true
 helm uninstall postgresql || true
 helm uninstall csi-driver-nfs -n kube-system || true
 
-echo -e "${GREEN}2. Eliminando todos los PVC...${NC}"
+echo -e "${GREEN}2. Eliminando pods de usuario JupyterHub..."
+kubectl delete pod -l component=singleuser-server || true
+sleep 10
+
+echo -e "${GREEN}3. Eliminando todos los PVC...${NC}"
 kubectl delete -f pvc/ --ignore-not-found
 kubectl get pvc --all-namespaces --no-headers | awk '{print "kubectl delete pvc", $2, "-n", $1}' | bash
 sleep 5
 
-echo -e "${GREEN}3. Eliminando todos los PV...${NC}"
+echo -e "${GREEN}4. Eliminando todos los PV...${NC}"
 kubectl delete -f pv/ --ignore-not-found
 kubectl get pv --no-headers | awk '{print "kubectl delete pv", $1}' | bash
 sleep 5
 
-echo -e "${GREEN}4. Eliminando StorageClass 'nfs-storage-sc' y 'nfs-csi-jupyterhub-sc'...${NC}"
+echo -e "${GREEN}5. Eliminando StorageClass 'nfs-storage-sc' y 'nfs-csi-jupyterhub-sc'...${NC}"
 kubectl delete -f storageclass/nfs-storage-sc.yaml --ignore-not-found
 kubectl delete -f storageclass/nfs-csi-jupyterhub-sc.yaml --ignore-not-found
 sleep 3
 
-echo -e "${GREEN}5. Eliminando CRDs de Ray...${NC}"
+echo -e "${GREEN}6. Eliminando CRDs de Ray...${NC}"
 kubectl delete crd rayclusters.ray.io rayjobs.ray.io rayservices.ray.io --ignore-not-found
 
 echo -e "${GREEN}✅ Desinstalación completada correctamente.${NC}"
