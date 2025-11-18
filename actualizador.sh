b1e9fe926cc10de243105db5968cc1992326ee57
Miguel <miguelsff@gmail.com>
2025-11-18 11:40:54 -0500

    add actualizador

---
+++ actualizador.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+set -e
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}1. Desinstalando componentes Helm...${NC}"
+helm uninstall raycluster || true
+helm uninstall jupyterhub || true
+
+echo -e "${GREEN}2. Eliminando pods de usuario JupyterHub...${NC}"
+kubectl delete pod -l component=singleuser-server --grace-period=0 --force || true
+sleep 10
+
+echo -e "${GREEN}✅ Desinstalación completada${NC}"
+
+# Configuración de variables
+variables_txt="variables.txt"
+current_context=$(kubectl config current-context)
+
+echo -e "${GREEN}🔹 Contexto actual: $current_context${NC}"
+
+if [[ "$current_context" == *deploy* ]]; then
+  entorno="SUNAT"
+else
+  entorno="AKS"
+  
+  # Configuración específica de AKS
+  redis_host="172.26.59.6"
+  redis_password="Sunat2025"
+
+  case "$current_context" in
+    deploy-Mlops-prod01|deploy-Mlops-produccion01|deploy-Mlops-prod02)
+      redis_host="172.26.59.5"
+      ;;
+    deploy-Mlops-Test01|deploy-Mlops-desa01)
+      redis_host="172.26.59.7"
+      ;;
+    deploy-Mlops-User01|deploy-Mlops-User02|deploy-Mlops-User03)
+      redis_host="172.26.59.6"
+      ;;
+  esac
+
+  # Actualizar variables.txt
+  sed -i "s|\${clusterName}|$current_context|g" "$variables_txt"
+  sed -i "s|\${redisHost}|$redis_host|g" "$variables_txt"
+  sed -i "s|\${redisPassword}|$redis_password|g" "$variables_txt"
+fi
+
+echo -e "${GREEN}3. Procesando variables para entorno $entorno...${NC}"
+
+# Procesar variables
+mapfile -t variables < "$variables_txt"
+
+for variable in "${variables[@]}"; do
+  IFS='|' read -r parte1 parte2 parte3 <<< "$variable"
+  
+  variable_name="\${${parte1#*:}}"
+  aks_val="${parte2#*:}"
+  sunat_val="${parte3#*:}"
+
+  if [[ "$entorno" == "AKS" ]]; then
+    valorvariable="$aks_val"
+  else
+    valorvariable="$sunat_val"
+  fi
+
+  echo "Reemplazando $variable_name por $valorvariable"
+  find pv pvc storageclass values -type f -name "*.yaml" -exec sed -i "s|$variable_name|$valorvariable|g" {} +
+done
+
+echo -e "${GREEN}4. Instalando JupyterHub...${NC}"
+helm upgrade --install jupyterhub charts/jupyterhub-3.3.8.tgz -f values/config-jupyterhub.yaml
+sleep 10 && helm status jupyterhub
+
+echo -e "${GREEN}5. Instalando Ray Cluster...${NC}"
+helm upgrade --install raycluster charts/ray-cluster-1.3.0.tgz -f values/config-ray.yaml
+sleep 10 && helm status raycluster
+
+echo -e "${GREEN}✅ Instalación completa en $entorno${NC}"
+echo -e "${GREEN}🔹 Contexto usado: $current_context${NC}"
