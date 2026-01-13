e650ad3c38b7447a944501b73e905f4c0f86ca98
Miguel <miguelsff@gmail.com>
2026-01-13 11:15:16 -0500

    update requirements ray and scypi images

--- images/miguelsff/ray-2.41.0-sunat/build-push.sh
+++ images/miguelsff/ray-2.41.0-sunat/build-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGISTRY=miguelsff
 IMAGE_NAME=ray
-TAG=2.41.0-py311-sunat-v7
+TAG=2.41.0-py311-sunat-v8
 
 docker build -t $REGISTRY/$IMAGE_NAME:$TAG .
 docker push $REGISTRY/$IMAGE_NAME:$TAG
